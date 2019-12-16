4ea993426c88c6615d34918d009f7ca45db0cbe1
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-17 00:44:14 +0300

    Добавлены мэппинги логических функций на соответствующией символы в InitializationService

--- src/main/java/services/InitializationService.java
+++ src/main/java/services/InitializationService.java
@@ -1,5 +1,6 @@
 package services;
 
+import expressions.logic.*;
 import expressions.math.Sum;
 import model.StringSymbol;
 import model.Symbol;
@@ -11,6 +12,11 @@ public class InitializationService {
     public static HashMap<StringSymbol, Function<Symbol[], Symbol>> generateExpressionMap(){
         HashMap<StringSymbol, Function<Symbol[], Symbol>> expressionMap = new HashMap<>();
         expressionMap.put(new StringSymbol("Sum"), Sum.getExpression());
+        expressionMap.put(LogicFunctions.If, If.getExpression());
+        expressionMap.put(LogicFunctions.And, And.getExpression());
+        expressionMap.put(LogicFunctions.Or, Or.getExpression());
+        expressionMap.put(LogicFunctions.Not, Not.getExpression());
+        expressionMap.put(LogicFunctions.Equal, Equal.getExpression());
         return expressionMap;
     }
 }
