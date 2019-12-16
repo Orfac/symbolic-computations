1d91cb85d17b852bd4617bd5d606f42739c6ad3d
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-16 20:43:50 +0300

    Обновлено выражение суммы

--- src/main/java/expressions/math/Sum.java
+++ src/main/java/expressions/math/Sum.java
@@ -1,5 +1,6 @@
 package expressions.math;
 
+import model.Constant;
 import model.Expression;
 import model.StringSymbol;
 import model.Symbol;
@@ -8,7 +9,14 @@ import java.util.function.Function;
 
 public class Sum {
     private Function<Symbol[], Symbol> expression =
-            symbols -> new Expression(new StringSymbol("Sum"), symbols );
+            symbols -> {
+                if (symbols[0] instanceof Constant && symbols[1] instanceof Constant){
+                    double left = ((Constant) symbols[0]).getValue();
+                    double right = ((Constant) symbols[1]).getValue();
+                    return new Constant(left + right);
+                }
+                return new Expression(new StringSymbol("Sum"), symbols );
+            };
 
     public Function<Symbol[], Symbol> getExpression() {
         return expression;
