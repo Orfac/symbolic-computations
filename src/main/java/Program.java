6364c68cd704516464d95aa84390eb3eed9ea570
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-16 22:20:38 +0300

    Смержен конфликт

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -13,7 +13,6 @@ import java.util.function.Function;
 
 public class Program {
     public static void main(final String[] args) {
-
 //        InputDto inputDto = fileService.getInputDto(args[0]);
 //
 //        System.out.println(inputDto);
@@ -25,20 +24,21 @@ public class Program {
         HashMap<StringSymbol,Symbol> symbolRules = new HashMap<>();
         symbolRules.put(
                 new StringSymbol("a"),
-                new Expression(new StringSymbol("+"), new Symbol[] {new StringSymbol("b"), new StringSymbol("c")}));
+                new Expression(new StringSymbol("Sum"), new Symbol[] {new StringSymbol("b"), new StringSymbol("c")}));
 
         HashMap<StringSymbol, Function<Symbol[], Symbol>> expressionRules = InitializationService.generateExpressionMap();
-        expressionRules.put(new StringSymbol("+"), function);
 
         EvaluationService service = new EvaluationService( symbolRules,expressionRules);
         Symbol exp = service.evaluateExpression(new Expression(new StringSymbol("List"), new Symbol[]{
                 new Expression(new StringSymbol("Set"),
                         new Symbol[]{
                                 new StringSymbol("e"),
-                                new Expression(new StringSymbol("+"),
-                                        new Symbol[]{new StringSymbol("b"),  new StringSymbol("c"),
-                                                new Expression(new StringSymbol("+"),
-                                                        new Symbol[]{new Constant(5),  new Constant(7)})}),
+                                new Expression(new StringSymbol("Sum"),
+                                        new Symbol[]{
+                                                new StringSymbol("b"),
+                                                new StringSymbol("c"),
+                                                new Expression(new StringSymbol("Sum"),
+                                                        new Symbol[]{new Constant(5),  new Constant(6)})}),
                         }),
                 new StringSymbol("e")
         }));
